f815786a7c86681a50a673afb07a7bd139b585e8
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-09 15:32:08 +0300

    create routes for new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 export default function App() {
 
     return (
-        <div></div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Homepage/>}/>
+                <Route path='product' element={<Product/>}/>
+                <Route path='price' element={<Pricing/>}/>
+                <Route path='*' element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
