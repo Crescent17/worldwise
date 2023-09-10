c4f50648bcbb50634485e18c58a27c8d713f96b7
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-10 17:59:17 +0300

    fetch visited cities from a fake API and display them in the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,30 @@ import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
+import {useEffect, useState} from "react";
+
+const BASE_URL = 'http://localhost:7000'
 
 export default function App() {
+    const [cities, setCities] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                setIsLoading(true)
+                const res = await fetch(`${BASE_URL}/cities`)
+                const data = await res.json()
+                setCities(data)
+            } catch (err) {
+                alert('There was an error loading data!')
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchCities()
+    }, []);
 
     return (
         <BrowserRouter>
@@ -17,8 +39,8 @@ export default function App() {
                 <Route path='pricing' element={<Pricing/>}/>
                 <Route path='login' element={<Login/>}/>
                 <Route path='app' element={<AppLayout/>}>
-                    <Route index element={<CityList/>}/>
-                    <Route path='cities' element={<CityList/>}/>
+                    <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route path='cities' element={<CityList cities={cities} isLoading={isLoading}/>}/>
                     <Route path='countries' element={<p>List of countries</p>}/>
                     <Route path='form' element={<p>Form</p>}/>
                 </Route>
