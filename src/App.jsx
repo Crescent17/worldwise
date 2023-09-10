0de524a55269080c364522afb315a5d4d5aebc95
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-10 13:17:03 +0300

    use CityList component in routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
+import CityList from "./components/CityList.jsx";
 
 export default function App() {
 
@@ -16,8 +17,8 @@ export default function App() {
                 <Route path='pricing' element={<Pricing/>}/>
                 <Route path='login' element={<Login/>}/>
                 <Route path='app' element={<AppLayout/>}>
-                    <Route index element={<p>List of cities</p>}/>
-                    <Route path='cities' element={<p>List of cities</p>}/>
+                    <Route index element={<CityList/>}/>
+                    <Route path='cities' element={<CityList/>}/>
                     <Route path='countries' element={<p>List of countries</p>}/>
                     <Route path='form' element={<p>Form</p>}/>
                 </Route>
