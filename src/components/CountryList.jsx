80a65a849b670e2214afb1751058165db47c09e7
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-10 19:52:43 +0300

    refactor code and give CountryItem a key

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -5,13 +5,16 @@ import CountryItem from "./CountryItem.jsx";
 
 export default function CountryList({cities, isLoading}) {
     const countries = cities.reduce((arr, city) => {
-        if (!arr.map(el => el.country).includes(city.country)) return [...arr, {country: city.country, emoji: city.emoji}]
+        if (!arr.map(el => el.country).includes(city.country)) return [...arr, {
+            country: city.country,
+            emoji: city.emoji
+        }]
         else return arr
     }, [])
     if (isLoading) return <Spinner/>
 
     if (cities.length === 0) return <Message message='Add your first city by clicking on a city on the map'/>
     return <ul className={styles.countryList}>
-        {countries.map(country => <CountryItem country={country}/>)}
+        {countries.map(country => <CountryItem key={country.country} country={country}/>)}
     </ul>
 }
