8c5a3652cd73256446fbfc0dd8339ff73080051a
Marat <94042173+Crescent17@users.noreply.github.com>
2023-09-14 16:04:27 +0300

    replace some object data in user properties and return context in useAuth hook

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -19,8 +19,8 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-    name: "Jack",
-    email: "jack@example.com",
+    name: "Marat",
+    email: "maratos2001@mail.ru",
     password: "qwerty",
     avatar: "https://i.pravatar.cc/100?u=zz",
 };
@@ -31,7 +31,7 @@ function AuthProvider({children}) {
     function login(email, password) {
         if (email === FAKE_USER.email && password === FAKE_USER.password) dispatch({
             type: 'login',
-            payload: {FAKE_USER}
+            payload: FAKE_USER
         })
     }
 
@@ -49,6 +49,7 @@ function AuthProvider({children}) {
 function useAuth() {
     const context = useContext(AuthContext)
     if (context === undefined) throw new Error('AuthContext was used outside of AuthProvider')
+    return context
 }
 
 export {AuthProvider, useAuth}
